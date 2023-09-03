16e7f528aa47833f56340b81d2bc6fe356d55705
Ayush <rishiustaad@gmail.com>
2023-09-04 01:35:41 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -6,22 +6,25 @@ import {FaSearch} from 'react-icons/fa'
 
 function App() {
 
-  
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q={location}&appid=00e5f67944d22b7b9c8236ad2467a404';
-
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
- 
 
+  
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=00e5f67944d22b7b9c8236ad2467a404`;
+
+  
 
   const searchLocation = (event) => {
-    if(event.key === 'Enter'){
-      axios.get(url).then((response) => {
-        setData(response.data);
-        console.log(response.data);
-      });
-      setLocation('');
-    }
+    event.preventDefault()
+    axios.get(url).then((response) => {
+      setData(response.data);
+      console.log(response.data);
+    }).catch((error) => {
+      console.log(error);
+    });
+      
+    setLocation('');
+    
   }
 
 
@@ -30,38 +33,46 @@ function App() {
 
   return (
     <div className="app">
-      <div className="search">
-        <input type="text" value={location} onChange={event => setLocation(event.target.value)} placeholder='Enter Location'/>
+      <form className="search" onSubmit={searchLocation}>
+        <input type='text'  value={location} onChange={event => setLocation(event.target.value)} placeholder='Enter Location'/>
         <button onClick={searchLocation}>
           <FaSearch className='search-icon'/>
         </button>
-      </div>
+      </form>
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Ranchi</p>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
-            <h1>50°F</h1>
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
           <div className="description">
-            Clouds
+            {data.weather ? data.weather[0].main : null}
           </div>
         </div>
-        <div className="bottom">
-          <div className="feels">
-            <p className='bold'>65°F</p>
-            <p>Feels Like</p>
-          </div>
-          <div className="humidity">
-            <p className="bold">20%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            <p className="bold">3MPH</p>
-            <p>Winds</p>
-          </div>
-        </div>
+        
+      {data.name != undefined && 
+      <div className="bottom">
+      <div className="feels">
+        <p className='bold'>
+          {data.main ? data.main.feels_like.toFixed() : null}°F
+        </p>
+        <p>Feels Like</p>
+      </div>
+      <div className="humidity">
+        <p className="bold">
+        {data.main ? data.main.humidity : null}%
+        </p>
+        <p>Humidity</p>
+      </div>
+      <div className="wind">
+        <p className="bold">
+          {data.clouds ? data.wind.speed : null} MPH
+        </p>
+        <p>Winds</p>
+      </div>
+    </div>}
       </div>
     </div>
   );
